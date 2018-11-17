cc4096a926c2d15350958779fdbb10aebbd92617
Cláudio Hilário <claudio.hilario@ontech.pt>
2018-11-17 12:07:42 +0000

    [ADD] Fila de Prioridades

--- Filas/filas.js
+++ Filas/filas.js
@@ -39,21 +39,79 @@
  }
 
  /** Teste da Class Queue */
- const fila = new Queue();
+//  const fila = new Queue();
 
- fila.enqueue('João');
- fila.enqueue('António');
- fila.enqueue('Manuel');
+//  fila.enqueue('João');
+//  fila.enqueue('António');
+//  fila.enqueue('Manuel');
 
- fila.print(); //Output: João,António,Manuel
+//  fila.print(); //Output: João,António,Manuel
 
- fila.dequeue();
- fila.print(); //Output: António,Manuel
+//  fila.dequeue();
+//  fila.print(); //Output: António,Manuel
 
- fila.dequeue();
- fila.print(); //Output: Manuel
+//  fila.dequeue();
+//  fila.print(); //Output: Manuel
 
- console.log(fila.isEmpty()); //Output: false
- console.log(fila.size()); //Output: 1
+//  console.log(fila.isEmpty()); //Output: false
+//  console.log(fila.size()); //Output: 1
 
+ /** Fila de prioridades */
  
+ class QueueElement {
+    constructor(element, priority) {
+        this.element = element;
+        this.priority = priority;
+    }
+}
+
+ class PriorityQueue {
+     constructor() {
+         this.items = [];
+     }
+
+     enqueue(element, priority) { //João, 2
+         const queueElement = new QueueElement(element, priority);
+         let added = false;
+
+         for(let i = 0; i < this.items.length; i++) {
+             if(queueElement.priority < this.items[i].priority) {
+                 this.items.splice(i, 0, queueElement);
+                 added = true;
+                 break;
+             }
+         }
+
+         if (!added) {
+             this.items.push(queueElement);
+         }
+     }
+
+     dequeue() {
+         return this.items.shift();
+     }
+
+     print() {
+        for(let i = 0; i < this.items.length; i++) {
+            console.log(`${this.items[i].element} ${this.items[i].priority}`)
+        }
+     }
+ }
+
+ //Teste Rápido
+
+ const pqueue = new PriorityQueue();
+
+ pqueue.enqueue('João', 2);
+ pqueue.enqueue('António', 1);
+ pqueue.enqueue('José', 3);
+ pqueue.enqueue('Manuel', 1);
+
+ pqueue.print();
+ /**
+  * Output:
+  *     - António 1
+  *     - Manuel 1
+  *     - João 2
+  *     - José 3
+  */
