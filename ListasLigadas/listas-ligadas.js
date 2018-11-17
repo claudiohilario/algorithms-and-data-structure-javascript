dc40faac06187705dbf5e496dbd76c45481bb110
Cláudio Hilário <claudio.hilario@ontech.pt>
2018-11-17 13:25:05 +0000

    [ADD] Estrutura base de Listas Ligadas

---
+++ ListasLigadas/listas-ligadas.js
@@ -0,0 +1,49 @@
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+    }
+
+    append(element) {
+        //Adiciona um elemento no final da lista
+    }
+
+    insert(position, element) {
+        //Adiciona um elemento em uma posição especifica.
+    }
+
+    removeAt(position) {
+        //Remove o elemento de uma posiçºao especifica
+    }
+
+    remove(element) {
+        // Remove o elemento element
+    }
+
+    indexOf(element) {
+        //Retorna a posição do elemento
+    }
+
+    isEmpty() {
+        //Retorna se está vazia ou não a instância
+    }
+
+    size() {
+        //Retorna o tamanho da instância 
+    }
+
+    toString() {
+        //Converte em string
+    }
+
+    print() {
+        //Imprime na consola
+    }
+}
