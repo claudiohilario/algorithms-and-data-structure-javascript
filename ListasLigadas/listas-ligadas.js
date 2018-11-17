3baa89bafa7121e3c4935b6b4407cac8bdadb7a5
Cláudio Hilário <claudio.hilario@ontech.pt>
2018-11-17 14:17:42 +0000

    [ADD] Método removeAt()

--- ListasLigadas/listas-ligadas.js
+++ ListasLigadas/listas-ligadas.js
@@ -35,8 +35,28 @@ class LinkedList {
         //Adiciona um elemento em uma posição especifica.
     }
 
+    //Remove o elemento de uma posiçºao especifica
     removeAt(position) {
-        //Remove o elemento de uma posiçºao especifica
+        if(position > -1 && position < this.length) {
+            let current = this.head;
+            let previous;
+            let index = 0;
+
+            if(position === 0) {
+                this.head = current.next;
+            } else {
+                while(index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+
+                previous.next = current.next;
+            }
+            this.length--;
+            return current.element;
+        } else {
+            return null;
+        }
     }
 
     remove(element) {
@@ -81,5 +101,5 @@ const ll = new LinkedList();
 ll.append('Claudio')
 ll.append('Daniel')
 ll.append('José')
-
 ll.print() // Output: Claudio Daniel José
+console.log(`Foi removido: ${ll.removeAt(0)}`);
