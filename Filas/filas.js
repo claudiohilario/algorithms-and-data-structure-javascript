5f9bbb3bf959de13d6d092d75a021e69a10c5d47
Cláudio Hilário <claudio.hilario@ontech.pt>
2018-11-17 10:25:39 +0000

    [ADD] Estrutura base da Classe Queue

---
+++ Filas/filas.js
@@ -0,0 +1,31 @@
+
+
+ class Queue {
+     constructor() {
+         this.items = [];
+     }
+
+     emqueue() {
+         //Adiciona um novo item
+     }
+
+     dequeue() {
+         //Remove um item
+     }
+
+     front() {
+         //Torna o primeiro elemento da fila.
+     }
+
+     isEmpty() {
+         //Verifica se a fila está vazia ou não
+     }
+
+     size() {
+         //Retorna o tamanho da fila 
+     }
+
+     print() {
+         //Imprimir a fila no console
+     }
+ }
