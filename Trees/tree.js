5548dc9c9c61fccd553e9f59a1a85020c0412bc7
Cláudio Hilário <claudio.hilario@ontech.pt>
2020-04-04 01:03:10 +0100

    [ADD] Tree insert method

--- Trees/tree.js
+++ Trees/tree.js
@@ -1,15 +1,40 @@
-class BinarySearchTree {
-    constructor() {
-        this.root = null;
-    }
-    
-    node(key) {
+class Node {
+    constructor(key) {
         this.key = key;
         this.left = null;
         this.right = null;
     }
+}
 
-    insert(key) {}
+function insertNode(node, newNode) {
+    if(newNode.key < node.key) {
+        if(node.left = null) {
+            node.left = newNode
+            return
+        }   
+        return insertNode(node.left, newNode);
+    } 
+    
+    if(node.right === null) {
+        node.right = newNode
+        return;
+    } 
+    return insertNode(node.right, newNode);
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(key) {
+        const newNode = new Node(key);
+        if(this.root === null) {
+            this.root = newNode;
+            return;
+        }
+        insertNode(root, newNode);
+    }
 
     search(key) {}
 
