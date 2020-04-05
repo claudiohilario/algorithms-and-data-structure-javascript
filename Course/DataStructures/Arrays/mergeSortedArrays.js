c87cd65fea5b220b012c88fb64e19e596da7e984
Cláudio Hilário <claudio.hilario@ontech.pt>
2020-04-05 17:20:51 +0100

    [ADD] merge sorted arrays

---
+++ Course/DataStructures/Arrays/mergeSortedArrays.js
@@ -0,0 +1,40 @@
+// mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]);
+// [ 0,  3,  4, 4, 6, 30, 31]
+
+const mergeSortedArrays = (arr1, arr2) => [...arr1, ...arr2].sort((a,b) => a-b);
+
+function mergeSortedArraysOne(arr1, arr2) {
+    const mergedArray = [];
+    let array1Item = arr1[0];
+    let array2Item = arr2[0];
+    let i = 1;
+    let j = 1;
+
+    // Check input
+    if(arr1.length === 0) {
+        return arr2;
+    }
+
+    if(arr2.length === 0) {
+        return arr1;
+    }
+
+    while (array1Item || array2Item) {
+        if(!array2Item || array1Item < array2Item) {
+            mergedArray.push(array1Item);
+            array1Item = arr1[i];
+            i++;
+        } else {
+            mergedArray.push(array2Item);
+            array2Item = arr2[j];
+            j++;
+        }
+    }
+
+    return mergedArray;
+}
+
+const arr1 = [0, 3, 4, 31];
+const arr2 = [4, 6, 30];
+
+console.log(mergeSortedArraysOne(arr1, arr2));
