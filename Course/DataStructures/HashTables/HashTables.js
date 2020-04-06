92fa506b34485f05de667ae2c8b5bc76bf3ea20a
Cláudio Hilário <claudio.hilario@ontech.pt>
2020-04-06 20:02:17 +0100

    [ADD] Hash tables keys

--- Course/DataStructures/HashTables/HashTables.js
+++ Course/DataStructures/HashTables/HashTables.js
@@ -33,10 +33,21 @@ class HashTable {
         return undefined;
     }
 
+    keys() {
+        const keysArray = [];
+        for(let i = 0; i < this.data.length; i++) {
+            if(this.data[i]) {
+                keysArray.push(this.data[i][0])
+            }
+        }
+        return keysArray;
+    }
+
 }
 
 const myHashTable = new HashTable(50);
 
 myHashTable.set('grapes', 1000);
 myHashTable.set('apples', 54);
-console.log(myHashTable.get('apples'));
+myHashTable.set('oranges', 2);
+console.log(myHashTable.keys());
