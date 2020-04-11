6cbea5aa447b56083ef4f36a9bbd28361f88ffe8
Cláudio Hilário <claudio.hilario@ontech.pt>
2020-04-11 02:26:16 +0100

    [ADD] firstRecurringCharacterTwo

--- Course/DataStructures/HashTables/FirstRecurringCharacter.js
+++ Course/DataStructures/HashTables/FirstRecurringCharacter.js
@@ -7,7 +7,7 @@
 
 // Given an array [2, 3, 4, 5]
 
-function test(arr) {
+function firstRecurringCharacter(arr) {
     const obj = {};
     let count = 0;
     let value = 0;
@@ -28,6 +28,36 @@ function test(arr) {
     return count > 1 ? value : undefined;
 }
 
-console.log(test([2, 3, 4, 5]));
+function firstRecurringCharacterOne(input) {
+    const aux = 0;
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            console.log(j, i);
+            if(input[i] === input [j]) {
+                return input[i];
+            }
+        }
+    }
+
+    return undefined;
+} // O(n^2)
+
+
+function firstRecurringCharacterTwo(input) {
+    const aux = 0;
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            console.log(j, i);
+            if(input[i] === input [j]) {
+                return input[i];
+            }
+        }
+    }
+
+    return undefined;
+} // O(n^2)
+
+
+console.log(firstRecurringCharacterOne([2, 1, 1, 2, 1, 1, 1]));
 
 
